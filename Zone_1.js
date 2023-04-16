063e7b7f53a11fecc2b4cad0c8b0e44afe989e94
Ceslaviee <gabin.simonard@hotmail.com>
2023-04-16 11:53:08 +0200

    a

--- Zone_1.js
+++ Zone_1.js
@@ -17,15 +17,19 @@ class Zone_1 extends Phaser.Scene {
         this.calque_tentative = this.carteDuNiveau.createLayer("tentative",this.tileset);
         this.calque_tentative.setCollisionByProperty({ Dur: true })
 
-        this.player = this.physics.add.sprite(0, 0, 'perso').setScale(0.3);
+        this.calque_switch = this.carteDuNiveau.createLayer("switch",this.tileset);
+        this.calque_switch.setCollisionByProperty({ Dur: true })
+        this.calque_switch.setVisible(false)
+
+        this.player = this.physics.add.sprite(50, 900, 'perso').setScale(0.3);
         this.player.setBounce(0.2);
         this.player.setCollideWorldBounds(true);
         this.cursors = this.input.keyboard.createCursorKeys();
-        this.physics.add.collider(this.player, this.calque_tentative);
         this.physics.world.setBounds(0, 0, 1600, 960);
         this.cameras.main.setBounds(0, 0, 1600, 960);
         this.cameras.main.startFollow(this.player);
         this.physics.add.collider(this.player, this.calque_tentative);
+        this.physics.add.collider(this.player, this.calque_switch,this.switch1, null, this );
         
 
     }
@@ -43,5 +47,9 @@ class Zone_1 extends Phaser.Scene {
             this.player.setVelocityY(-330);
         }
 
+    }
+    switch1() 
+    {
+        this.scene.start("Zone_2");
     }
 };
