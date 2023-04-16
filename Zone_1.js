19bf1d7217ebb0015f4f9d14bde28edb3762a482
Ceslaviee <gabin.simonard@hotmail.com>
2023-04-16 23:57:34 +0200

    bisou

--- Zone_1.js
+++ Zone_1.js
@@ -12,7 +12,7 @@ class Zone_1 extends Phaser.Scene {
         this.load.tilemapTiledJSON("Jardin", "Json/Zone_1.json");
         this.load.image("fond_1","doc/galaxie.png")
         this.load.image('perso', 'doc/Gaïa.png',{ frameWidth: 32, frameHeight: 65 });
-        this.load.image('fin', "doc/fin.png");
+        this.load.image('soleil', "planetes/Soleil.png");
         this.load.audio('Dead_Ends', "son/Dead_Ends.mp3");
         this.load.spritesheet('final', "doc/final.png",{ frameWidth: 971, frameHeight: 143})
         
@@ -44,6 +44,14 @@ class Zone_1 extends Phaser.Scene {
         this.calque_chute.setCollisionByProperty({ Dur: true })
         this.calque_chute.setVisible(false)
 
+        this.calque_point = this.carteDuNiveau.getObjectLayer("point")
+        this.pickup = this.physics.add.group({
+            gravity : false
+        })
+        this.calque_point.objects.forEach(calque_point => {
+            const POcol = this.pickup.create(calque_point.x, calque_point.y, "soleil").setScale(0.3).body.setAllowGravity(false);
+        });
+
 
         //Config
         this.player = this.physics.add.sprite(this.spawnX, this.spawnY, 'perso').setScale(0.3);
@@ -56,6 +64,12 @@ class Zone_1 extends Phaser.Scene {
         this.physics.add.collider(this.player, this.calque_tentative);
         this.physics.add.collider(this.player, this.calque_switch,this.switch1, null, this );
         this.physics.add.collider(this.player, this.calque_chute,this.respawn, null, this );
+        this.physics.add.overlap(this.player, this.pickup, this.upScore, null, this );
+
+        this.score = 0
+        this.scoreTexte = this.add.text(880, 40, this.score, {
+            fontSize : '32px', fill : "#000"
+        }).setScrollFactor(0)
 
 
         this.anims.create({
@@ -65,6 +79,7 @@ class Zone_1 extends Phaser.Scene {
             
         });
         this.hf.anims.play('1', true)
+
 
         
 
@@ -100,6 +115,14 @@ class Zone_1 extends Phaser.Scene {
             this.player.setVelocityY(-330);
         }
 
+
+    }
+
+    upScore(player,pickup)
+    {
+        pickup.destroy()
+        this.score += 1
+        this.scoreTexte.setText(this.score)
     }
     switch1() 
     {
